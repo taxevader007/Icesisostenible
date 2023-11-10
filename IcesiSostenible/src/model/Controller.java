152c84db22198fd66adeec7016a7c96d2214c65e
Santiago Espinosa <santiagoespinosagiraldo1@gmail.com>
2023-11-10 08:10:16 -0500

    adding and examples from the past TI2

--- IcesiSostenible/src/model/Controller.java
+++ IcesiSostenible/src/model/Controller.java
@@ -44,33 +44,71 @@ public class Controller {
         
         Project project = new Project(researchername, pilarType, proyectStatus, proyectName, proyectIdentifier, proyectDescription, initialDate, endDate);
         
-        switch(pilarType){
-            case "biodiversity" -> pilars[0].addProject(project);
-            case "water" -> pilars[1].addProject(project);
-            case "trashcollector" -> pilars[2].addProject(project);
-            case "energy" -> pilars[3].addProject(project);
-            default -> {
-                return "invalid pilar";
-            }
-        }
+		if (researchername.equals(project.getResearchername()) && pilarType.equals(project.getPilarType()))
+			switch(pilarType.toLowerCase()){
+				case "biodiversity" -> pilars[0].addProject(project);
+				case "water" -> pilars[1].addProject(project);
+				case "trashcollector" -> pilars[2].addProject(project);
+				case "energy" -> pilars[3].addProject(project);
+				default -> {
+					return "cannot add proyect if not researcher and a pilar ";
+				}
+			}
 
-        return "cannot add proyect if not researcher";
+        return "proyect added successfully";
         
 	}
 
-    public String modifyProyect(String researchername, String pilarType, String proyectStatus, String proyectName, String proyectIdentifier, String proyectDescription, Calendar initialDate, Calendar endDate) {
+    public String modifyProyect(String researchername, String pilarType, String newproyectStatus, String newproyectName, String newproyectIdentifier, String newproyectDescription, Calendar newinitialDate, Calendar newendDate) {
+
+		
+
+			
         
-        
+		//public void editPlanet(String galaxyName, String planetName, String newPlanetName, int newNumberOfSatellites, double newPlanetRadius, double newPlanetMass) {
+        //for (int i = 0; i < galaxyCounter; i++) {
+          //  if (galaxyMemory[i] != null && galaxyMemory[i].getGalaxyName().equals(galaxyName)) {
+          //      Galaxy associatedGalaxy = galaxyMemory[i];
+    	//	
+    	//            for (int j = 0; j < associatedGalaxy.getGalaxyPlanets().length; j++) {
+          //          if (associatedGalaxy.getGalaxyPlanets()[j] != null && associatedGalaxy.getGalaxyPlanets()[j].getPlanetName().equals(planetName)) {
+        //                associatedGalaxy.getGalaxyPlanets()[j].setPlanetName(newPlanetName);
+          //              associatedGalaxy.getGalaxyPlanets()[j].setPlanetNumberSatelites(newNumberOfSatellites);
+            //            associatedGalaxy.getGalaxyPlanets()[j].setPlanetRadio(newPlanetRadius);
+          //              associatedGalaxy.getGalaxyPlanets()[j].setPlanetMass(newPlanetMass);
+            //            break;
+             //       }
+             //   }
+        //    }
+        //}
+    
+			
 
 		return "Proyect modified successfully";
 	}
 
 	public void removeProyect() {
-
+	//* public void rmPlanet( String galaxyName, String planetName) {
+    //    for (int i = 0; i < galaxyCounter; i++) {
+    //        if (galaxyMemory[i] != null && galaxyMemory[i].getGalaxyName().equals(galaxyName)) {
+    //            Galaxy associatedGalaxy = galaxyMemory[i];
+    //
+    //            for (int j = 0; j < associatedGalaxy.getGalaxyPlanets().length; j++) {
+    //                if (associatedGalaxy.getGalaxyPlanets()[j] != null && associatedGalaxy.getGalaxyPlanets()[j].getPlanetName().equals(planetName)) {
+    //                    associatedGalaxy.getGalaxyPlanets()[j] = null;
+    //                    break;
+    //                }
+    //            }
+    //       }
+    //    }
+    //}
+	// */	
 		
 	}
 
 	public void registerEvidence() {
+
+
 		
 	}
 
